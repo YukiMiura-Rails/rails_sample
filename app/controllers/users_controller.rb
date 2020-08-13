b94b00b52258335eb12a3dd9b5ac61d229677fbd
YukiMiura <koco345@gmail.com>
2020-08-13 13:50:45 +0000

    finished  half cp7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  # get users/new
   def new
+    @user = User.new
   end
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+  def create
+   # debugger
+   @user = User.new(user_params)
+   if @user.save
+    #success (valid params)
+   else
+    #failure (invalid params)
+    render 'new'
+   end
+  end
+ 
+  private 
+ 
+  def user_params
+    params.require(:user).permit(:name, :email,
+   :password ,:password_confirmation)
+  end
+
+
 end
