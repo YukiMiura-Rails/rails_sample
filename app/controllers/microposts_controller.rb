5b0473c04a0413792865d6a178203d0f84ff4b8a
YukiMiura <koco345@gmail.com>
2020-08-28 03:28:55 +0000

    finished cp13 skipped 13.4 and image resize

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -26,7 +26,7 @@ class MicropostsController < ApplicationController
       params.require(:micropost).permit(:content)
     end
     def correct_user
-      @micropost = current_user.micropostsml.find_by(id: params[:id])
+      @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to(root_url) if @micropost.nil?
      end
      def micropost_params
