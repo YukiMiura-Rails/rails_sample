acd50deaa87e615a93d8b8255c18df10a542c16e
YukiMiura <koco345@gmail.com>
2020-09-03 06:18:29 +0000

    cp14 half

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
    attr_accessor :remember_token, :activation_token , :reset_token
    has_many :microposts, dependent: :destroy
+   has_many :active_relationships, class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+   has_many :passive_relationships, 
+                                 class_name: "Relationship", 
+                                 foreign_key: "followed_id",
+                                 dependent: :destroy
+   has_many :following, through: :active_relationships, source: :followed
+   has_many :followers, through: :passive_relationships ,source: :follower
    before_save :downcase_email
    before_create :create_activation_digest
    has_secure_password
@@ -67,6 +76,19 @@ class User < ApplicationRecord
    end
    def feed
       Micropost.where("user_id=?",id)
+      
+   end
+   def follow(other_user)
+      following << other_user
+   end
+   
+   def unfollow(other_user)
+   active_relationships.find_by(followed: other_user.id).destroy
+
+      
+   end
+   def following?(other_user)
+      following.include?(other_user)
       
    end
    
